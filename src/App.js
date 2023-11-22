9c2954bddf3cb9c1acede3d578bb7757240478ed
Prithigacs <132645154+Prithigacs@users.noreply.github.com>
2023-11-22 10:50:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,185 @@
+import React, { Component } from "react";
+import firebase from "firebase";
+import TimeTable from "./components/TimeTable.js";
+import ShowDetail from "./components/ShowDetail.js";
+import Adder from "./components/Adder.js";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    /*
+     * edit this config to sync with firebase
+     * copy from firebase project
+     */
+    this.config = {
+      apiKey: "<API key>",
+      authDomain: "<Auth Domain>",
+      databaseURL: "<Database URL>",
+      projectId: "<Project ID>",
+      storageBucket: "<Bucket>",
+      messagingSenderId: "<Sender ID>"
+    };
+    firebase.initializeApp(this.config);
+    /*
+     * table : contain schedule of each day , in each contain id of subject
+     * detail : contain detail of each subject
+     * id : use to generate new subject
+     */
+    this.state = {
+      a: -72,
+      table: {
+        // TODO: use constants instead of repeating array size and init value.
+        mon: Array(26).fill(-1),
+        tue: Array(26).fill(-1),
+        wed: Array(26).fill(-1),
+        thu: Array(26).fill(-1),
+        fri: Array(26).fill(-1),
+        sat: Array(26).fill(-1),
+        sun: Array(26).fill(-1)
+      },
+      detail: {},
+      id: 0
+    };
+  }
+
+  componentDidMount() {
+    // First, read data from database
+    firebase
+      .database()
+      .ref("schedule/")
+      .once("value")
+      .then(snapshot => {
+        // if we doesn't have detail in database, we will first add dummy data to database
+        if (snapshot.val().detail == undefined) {
+          let dummy = {
+            "-3": {
+              day: "mon",
+              subject: "Example",
+              sub_detail: "Don't delete this.",
+              id: -3,
+              color: "#ffaaaa"
+            }
+          };
+          firebase
+            .database()
+            .ref()
+            .update({ "schedule/detail": dummy });
+        }
+        // set data from database to state
+        this.setState({
+          table: snapshot.val().table,
+          id: snapshot.val().id,
+          detail: snapshot.val().detail
+        });
+      });
+  }
+  changeTable = (d, start, end) => {
+    var tmpT = this.state.table;
+    for (var i = start; i < end; i++) {
+      if (tmpT[d][i] >= 0) {
+        return false;
+      }
+      tmpT[d][i] = this.state.id;
+    }
+
+    this.setState({ table: tmpT });
+
+    return true;
+  };
+
+  addDetail = (date, sub, det, col) => {
+    var idx = this.state.id;
+    var tmpList_detail = this.state.detail;
+    tmpList_detail[idx] = {
+      day: date,
+      subject: sub,
+      sub_detail: det,
+      id: idx,
+      color: col
+    };
+    this.setState({ detail: tmpList_detail });
+  };
+
+  delDetail = (day, id) => {
+    var tmp_del1 = this.state.detail;
+    var tmp_del2 = this.state.table;
+
+    for (var k = 0; k < this.state.table[day].length; k++) {
+      if (id == tmp_del2[day][k]) {
+        tmp_del2[day][k] = -1;
+      }
+    }
+    for (let a in tmp_del1) {
+      if (id == a) {
+        delete tmp_del1[a];
+      }
+    }
+    this.setState({ detail: tmp_del1, table: tmp_del2 });
+  };
+
+  getDetail = id => {
+    let out1 = "";
+    let out2 = "";
+    for (let a in this.state.detail) {
+      if (id == a) {
+        out1 = this.state.detail[a].subject;
+        out2 = this.state.detail[a].color;
+      }
+    }
+    return [out1, out2];
+  };
+
+  incrementId = () => {
+    var tmp_id = this.state.id;
+    tmp_id += 1;
+    this.setState({ id: tmp_id });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <nav
+          class="navbar navbar-dark bg-dark"
+          style={{ position: "fixed", width: "100%", top: "0" }}
+        >
+          <a href="#" class="badge badge-primary">
+            S
+          </a>
+          <h3 style={{ color: "white", textAlign: "left" }}>Scheduler</h3>
+        </nav>
+        <Adder
+          changeTable={this.changeTable}
+          addDetail={this.addDetail}
+          id={this.state.id}
+          incrementId={this.incrementId}
+        />
+        <ShowDetail detail={this.state.detail} delDetail={this.delDetail} />
+        <TimeTable
+          table={this.state.table}
+          detail={this.state.detail}
+          getDetail={this.getDetail}
+          delDetail={this.delDetail}
+        />
+        <button
+          style={{ margin: "20px" }}
+          className="btn btn-outline-primary"
+          onClick={() => {
+            var updates = {};
+            updates["/schedule/detail"] = this.state.detail;
+            updates["/schedule/id"] = this.state.id;
+            updates["/schedule/table"] = this.state.table;
+            firebase
+              .database()
+              .ref()
+              .update(updates);
+          }}
+        >
+          Save
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Adder.js
@@ -0,0 +1,217 @@
+import React, { Component } from "react";
+
+const DEFAULT_STATE = {
+  day: "mon", subj: "", detail: "",
+  h1: 8, m1: 0, h2: 8, m2: 30,
+  color: "#ff55ff"
+};
+
+// This class use to make part of input to add subject to table
+class Adder extends Component {
+  constructor() {
+    super();
+    // state keep temporary data
+    this.state = DEFAULT_STATE;
+    // encoder change time to index
+    this.encoder = {
+      "8 0": 0,
+      "8 30": 1,
+      "9 0": 2,
+      "9 30": 3,
+      "10 0": 4,
+      "10 30": 5,
+      "11 0": 6,
+      "11 30": 7,
+      "12 0": 8,
+      "12 30": 9,
+      "13 0": 10,
+      "13 30": 11,
+      "14 0": 12,
+      "14 30": 13,
+      "15 0": 14,
+      "15 30": 15,
+      "16 0": 16,
+      "16 30": 17,
+      "17 0": 18,
+      "17 30": 19,
+      "18 0": 20,
+      "18 30": 21,
+      "19 0": 22,
+      "19 30": 23,
+      "20 0": 24,
+      "20 30": 25,
+      "21 0": 26,
+      "21 30": 26
+    };
+  }
+
+  // random color for each subject
+  randomColor = () => {
+    let out = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    console.log(out);
+    return out;
+  };
+
+  // function wrap randomColor
+  changeColor = () => {
+    this.setState({ color: this.randomColor() });
+  };
+
+  addEntry = () => {
+    if (this.state.subj == "") {
+      alert("subject field is required");
+      return;
+    }
+
+    let start = this.encoder[this.state.h1 + " " + this.state.m1];
+    let end = this.encoder[this.state.h2 + " " + this.state.m2];
+
+    if (start >= end) {
+      alert("start time must be earlier than end time");
+      return;
+    }
+
+    if (this.props.changeTable(this.state.day, start, end)) {
+      this.props.addDetail(
+        this.state.day,
+        this.state.subj,
+        this.state.detail,
+        this.state.color
+      );
+      this.props.incrementId();
+    } else {
+      alert("this time slot is already occupied");
+    }
+  }
+
+  render() {
+    return (
+      <div style={{ margin: "20px", marginTop: "100px" }}>
+        <h1>
+          <b>Fill the time table information</b>
+        </h1>
+        <div className="adder">
+          <h5>
+            Select Date:{"  "}
+            <select
+              name="day"
+              onChange={e => {
+                this.setState({ day: e.target.value });
+              }}
+              value={this.state.day}
+              style={{ maxWidth: "100%" }}
+            >
+              <option value="mon">Mon</option>
+              <option value="tue">Tue</option>
+              <option value="wed">Wed</option>
+              <option value="thu">Thu</option>
+              <option value="fri">Fri</option>
+              <option value="sat">Sat</option>
+              <option value="sun">Sun</option>
+            </select>
+          </h5>
+
+          <h5>
+            Subject:{"  "}
+            <input
+              style={{ maxWidth: "100%" }}
+              id="sub"
+              type="text"
+              onChange={e => {
+                console.log(e.target.value);
+                this.setState({ subj: e.target.value });
+              }}
+              value={this.state.subj}
+            />
+          </h5>
+
+          <h5>
+            Detail:{"  "}
+            <input
+              style={{ maxWidth: "100%" }}
+              id="det"
+              type="text"
+              onChange={e => {
+                this.setState({ detail: e.target.value });
+              }}
+              value={this.state.detail}
+            />
+          </h5>
+
+          <h5>
+            Time:{"  "}
+            <input
+              id="h1"
+              type="number"
+              min="8"
+              max="20"
+              step="1"
+              value={this.state.h1}
+              onChange={e => {
+                this.setState({ h1: e.target.value });
+              }}
+            />{" "}
+            <input
+              id="m1"
+              type="number"
+              min="0"
+              max="30"
+              step="30"
+              value={this.state.m1}
+              onChange={e => {
+                this.setState({ m1: e.target.value });
+              }}
+            />{" "}
+            to{" "}
+            <input
+              id="h2"
+              type="number"
+              min="8"
+              max="21"
+              step="1"
+              value={this.state.h2}
+              onChange={e => {
+                this.setState({ h2: e.target.value });
+              }}
+            />{" "}
+            <input
+              id="m2"
+              type="number"
+              min="0"
+              max="30"
+              step="30"
+              value={this.state.m2}
+              onChange={e => {
+                this.setState({ m2: e.target.value });
+              }}
+            />
+          </h5>
+          <div className="adder-button">
+            <button
+              className="btn btn-outline-primary"
+              onClick={this.addEntry}
+            >
+              Add
+            </button>
+            {"          "}
+            <button
+              className="btn btn-outline-success"
+              onClick={this.changeColor}
+            >
+              ChangeColor
+            </button>
+            {"         "}
+            <button
+              className="btn btn-outline-dark"
+              onClick={this.setState(DEFAULT_STATE)}
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Adder;
